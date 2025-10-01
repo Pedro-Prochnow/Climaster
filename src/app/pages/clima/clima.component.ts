47e46decab9a79a73d7311d92aa4722a353433dd
eleandrovanelli <eleandro.vanelli@ciss.com.br>
2025-09-30 22:11:11 -0300

    mais coisa

--- src/app/pages/clima/clima.component.ts
+++ src/app/pages/clima/clima.component.ts
@@ -1,22 +1,35 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { OpenWeatherService } from '../../service/open-weather.service';
+import { WeatherResponse } from '../../models/wheater-response.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of } from 'rxjs';
+import { DecimalPipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clima',
+  standalone: true,
+  imports: [DecimalPipe, TitleCasePipe],
   templateUrl: './clima.component.html',
   styleUrls: ['./clima.component.scss']
 })
-export class ClimaComponent implements OnInit {
+export class ClimaComponent {
 
   navegador = inject(Router);
   openWeather = inject(OpenWeatherService);
 
-  constructor() { }
+  dadosClima = toSignal<WeatherResponse | null>(
+    this.openWeather.buscarInfoClimaCidadeAtual()
+    .pipe(
+      catchError(err => {
+        console.error('Erro ao buscar dados do clima', err);
+        return of(null);
+      })
+    ),
+    {initialValue: null}
+  );
 
-  ngOnInit() {
-    this.openWeather.buscarInfoClimaCidadeAtual();
-  }
+  constructor() { }
 
   navegarParaTelaDePesquisa() {
     this.navegador.navigate(['/pesquisa']);
---
+++ src/app/service/open-weather.service.ts
@@ -0,0 +1,26 @@
+import {inject, Injectable} from '@angular/core';
+import {WeatherResponse} from '../models/wheater-response.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenWeatherService {
+
+  private apiKey = '2404bcd477c873621f66300650daaff2';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private http = inject(HttpClient);
+
+  constructor() {
+  }
+
+  buscarInfoClimaCidadeAtual(): Observable<WeatherResponse> {
+
+    const urlCompleta =
+      this.apiUrl + '?q=Dois%20Vizinhos,PR,BR&appid='
+      + this.apiKey + '&lang=pt_br&units=metric';
+
+    return this.http.get<WeatherResponse>(urlCompleta);
+  }
+}
